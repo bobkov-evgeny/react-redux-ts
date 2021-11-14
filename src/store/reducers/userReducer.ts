55f982a8389b012e5acb5641c40efb25832e05a6
Evgenii <freeedayy@gmail.com>
2021-11-14 21:42:10 +0300

    implemented reducers with TS

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,17 +1,4 @@
-
-const FETCH_USERS = 'FETCH_USERS';
-const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
-const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR';
-
-interface UserState {
-    users: any[];
-    loading: boolean;
-    error: null | string;
-}
-interface UserAction {
-    type: string;
-    payload?: any;
-}
+import {UserAction, UserActionTypes, UserState} from "../../types/user";
 
 const initialState: UserState = {
     users: [],
@@ -21,11 +8,11 @@ const initialState: UserState = {
 
 export const userReducer = (state = initialState, action: UserAction): UserState => {
     switch (action.type) {
-        case FETCH_USERS:
+        case UserActionTypes.FETCH_USERS:
             return { loading: true, error: null, users: []}
-        case FETCH_USERS_SUCCESS:
+        case UserActionTypes.FETCH_USERS_SUCCESS:
             return { loading: false, error: null, users: action.payload}
-        case FETCH_USERS_ERROR:
+        case UserActionTypes.FETCH_USERS_ERROR:
             return { loading: false, error: action.payload, users: []}
         default:
             return state
